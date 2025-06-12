26c728cc1cb58b4c290396f3df9978386c945c2c
Mikita Shapel <shapelnik@mail.ru>
2025-06-12 16:17:25 +0300

    Temaplates for db migrators.

--- src/main/java/com/leverx/learningmanagementsystem/core/db/service/CloudDatabaseMigrator.java
+++ src/main/java/com/leverx/learningmanagementsystem/core/db/service/CloudDatabaseMigrator.java
@@ -1,4 +1,14 @@
 package com.leverx.learningmanagementsystem.core.db.service;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@AllArgsConstructor
+@Profile("cloud")
 public class CloudDatabaseMigrator {
+        // TODO
 }
--- src/main/java/com/leverx/learningmanagementsystem/multitenancy/migrator/CloudMultitenantMigrator.java
+++ src/main/java/com/leverx/learningmanagementsystem/multitenancy/migrator/CloudMultitenantMigrator.java
@@ -10,4 +10,5 @@ import org.springframework.stereotype.Component;
 @Profile("cloud")
 @Slf4j
 public class CloudMultitenantMigrator {
+    // TODO
 }
