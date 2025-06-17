88b0e40f51eeab103a5ebdf28cf0418063798338
Mikita Shapel <shapelnik@mail.ru>
2025-06-17 11:30:11 +0300

    Refactor CloudTenantInfoFilter.

--- src/main/java/com/leverx/learningmanagementsystem/core/security/filter/CloudTenantInfoFilter.java
+++ src/main/java/com/leverx/learningmanagementsystem/core/security/filter/CloudTenantInfoFilter.java
@@ -24,6 +24,8 @@ public class CloudTenantInfoFilter extends OncePerRequestFilter {
 
     public static final String BEARER_PREFIX = "Bearer ";
     public static final String ZID = "zid";
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String API_NAME = "-lms";
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -33,29 +35,45 @@ public class CloudTenantInfoFilter extends OncePerRequestFilter {
             return;
         }
 
-        String serverName = request.getServerName();
-        String subdomain = serverName.substring(0, serverName.indexOf("-lms"));
+        String subdomain = extractTenantSubdomain(request);
         log.info("Tenant subdomain: {}", subdomain);
 
-        String bearerToken = request.getHeader("Authorization");
-        if (nonNull(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
-            String token = bearerToken.substring(BEARER_PREFIX.length());
-            log.info("Bearer token: {}", token);
-            DecodedJWT decodedJWT = JWT.decode(token);
-            log.info("DecodedJWT payload: {}", decodedJWT.getPayload());
-            String tenantId = decodedJWT.getClaim(ZID).asString();
-            log.info("TenantId: {}", tenantId);
-            TenantContext.setTenantId(tenantId);
-            TenantContext.setTenantSubdomain(subdomain);
-        } else {
-            log.info("Bearer token is null or invalid");
-            throw new TenantException("Authorization header is missing");
-        }
+        String tenantId = extractTenantId(request);
+        log.info("Tenant id: {}", tenantId);
+
+        setTenantContext(tenantId, subdomain);
 
         try {
             filterChain.doFilter(request, response);
         } finally {
             TenantContext.clear();
         }
+    }
+
+    private String extractTenantId(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION);
+
+        if (nonNull(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+            String token = bearerToken.substring(BEARER_PREFIX.length());
+            log.info("Bearer token: {}", token);
+
+            DecodedJWT decodedJWT = JWT.decode(token);
+            log.info("DecodedJWT payload: {}", decodedJWT.getPayload());
+
+            return decodedJWT.getClaim(ZID).asString();
+        } else {
+            log.info("Bearer token is null or invalid");
+            throw new TenantException("Authorization header is missing");
+        }
+    }
+
+    private String extractTenantSubdomain(HttpServletRequest request) {
+        String serverName = request.getServerName();
+        return serverName.substring(0, serverName.indexOf(API_NAME));
+    }
+
+    private void setTenantContext(String tenantId, String tenantSubdomain) {
+        TenantContext.setTenantId(tenantId);
+        TenantContext.setTenantSubdomain(tenantSubdomain);
     }
 }
