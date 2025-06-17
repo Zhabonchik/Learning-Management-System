698551e0a7c8b084515656ff7830ce9c90f9a8f5
Mikita Shapel <shapelnik@mail.ru>
2025-06-17 13:49:48 +0300

    Remove unused imports.

--- src/main/java/com/leverx/learningmanagementsystem/btp/registry/service/impl/CloudSubscriptionService.java
+++ src/main/java/com/leverx/learningmanagementsystem/btp/registry/service/impl/CloudSubscriptionService.java
@@ -23,13 +23,9 @@ import java.util.Map;
 
 import static com.leverx.learningmanagementsystem.btp.registry.constants.RegistryConstants.BINDING;
 import static com.leverx.learningmanagementsystem.btp.registry.constants.RegistryConstants.DB_NAME;
-import static com.leverx.learningmanagementsystem.btp.registry.constants.RegistryConstants.INSTANCE_CREATION_TIMEOUT_SEC;
-import static com.leverx.learningmanagementsystem.btp.registry.constants.RegistryConstants.INSTANCE_DELETION_TIMEOUT_SEC;
 import static com.leverx.learningmanagementsystem.btp.registry.constants.RegistryConstants.ROUTER_URL;
 import static com.leverx.learningmanagementsystem.btp.registry.constants.RegistryConstants.SCHEMA;
-import static com.leverx.learningmanagementsystem.btp.registry.constants.RegistryConstants.SERVICE_BINDING_TIMEOUT_SEC;
 import static com.leverx.learningmanagementsystem.btp.registry.constants.RegistryConstants.SERVICE_PLAN_ID;
-import static com.leverx.learningmanagementsystem.btp.registry.constants.RegistryConstants.SERVICE_UNBINDING_TIMEOUT_SEC;
 import static com.leverx.learningmanagementsystem.btp.registry.constants.RegistryConstants.TENANT_ID;
 
 @Service
